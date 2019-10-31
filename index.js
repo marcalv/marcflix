bb713c35eb7ab62cf05f4ab9ef6225d58ec1cc7b
marcalv <alvarezbernalmarc@gmail.com>
2019-10-31 17:28:12 +0100

    remove telegram app use

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const keyboard = Markup.inlineKeyboard([
 const bot = new Telegraf('975230773:AAGLCmIVgZWzEItFoLrkF_9eV5-ZFz4Qlio')
 //907510217:AAE1ST2jZ2NDQo0t8Y8Uv5qbVP2CDSroGx8
 //975230773:AAGLCmIVgZWzEItFoLrkF_9eV5-ZFz4Qlio
-app.use(bot.webhookCallback('/secret-path'))
+//app.use(bot.webhookCallback('/secret-path'))
 //bot.telegram.setWebhook('https://tormarc.herokuapp.com/secret-path')
 bot.start((ctx) => ctx.reply('Hello'))
 //bot.help((ctx) => ctx.reply('Help message'))
