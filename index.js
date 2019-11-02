0fbeae31ccd095d740bc221d54fac5307375aee2
marcalv <alvarezbernalmarc@gmail.com>
2019-11-02 20:00:21 +0100

    super update

--- index.js
+++ index.js
@@ -7,25 +7,46 @@ const fs = require('fs-extra');
 const os = require('os');
 const checkDiskSpace = require('check-disk-space')
 const glob = require('glob');
-const RSS = require('rss');
-var xml = require('xml');
 var isVideo = require('is-video');
 const parseTorrent = require('parse-torrent')
 const axios = require('axios')
 
+
+
+//================================================================================
+// CONST
+//================================================================================
+
+const PORT = process.env.PORT;
+const uptime_api_key = process.env.UPTIME_KEY
+const uptime_id = process.env.UTPIME_ID
+const bot_token = process.env.BOT_TOKEN
+const DEBUG = (process.env.DEBUG || 'missing debug env var' == "True") ? true : false
+const HOSTNAME = process.env.HOSTNAME || 'http://miair.lan:8000';
+
+//================================================================================
+//WEBSERVER SETUP 
+//================================================================================
+
 //Here we are configuring express to use body-parser as middle-ware.
-//app.use(bodyParser.urlencoded({ extended: false }));
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-var jsonParser = bodyParser.json();
 
 //Handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
+//Set static folder
+app.use(express.static(path.join(__dirname,'public'))) 
 
+//Start server
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+
+console.log("======================================")
+
+
+//================================================================================
 //TELEGRAM BOT
-
-
+//================================================================================
 
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
@@ -35,14 +56,12 @@ const keyboard = Markup.inlineKeyboard([
   Markup.urlButton('Open MarcFlix', 'https://tormarc.herokuapp.com'),
 ])
 
+const bot = new Telegraf(bot_token)
+//app.use(bot.webhookCallback('/secret-path'))
+//bot.telegram.setWebhook('https://tormarc.herokuapp.com/secret-path')
 
-
-const bot = new Telegraf('975230773:AAGLCmIVgZWzEItFoLrkF_9eV5-ZFz4Qlio')
-app.use(bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook('https://tormarc.herokuapp.com/secret-path')
-
-//bot.start((ctx) => ctx.reply('Hello'))
-//bot.help((ctx) => ctx.reply('Help message'))
+bot.start((ctx) => ctx.reply('Hello'))
+bot.help((ctx) => ctx.reply('Help message'))
 bot.on('message', (ctx) => {
   console.log('Message from '+ctx.message.from.username+' '+ctx.message.from.id)
 
@@ -56,85 +75,90 @@ bot.on('message', (ctx) => {
       ctx.reply('not magnet')
     }
   }
-
-  
-  
-  })
+})
 bot.launch()
 
-//ENDING -------- TELEGRAM BOT
-
+//================================================================================
 //Torrent
-var torrentDir = ''
-createTorrentDir()
-//torrentDir = path.join(__dirname,'torrents')
+//================================================================================
 
+//Create torrent download folder
+if (DEBUG) {
+  torrentDir = path.join(__dirname,'torrents')
+}else{
+  var torrentDir = ''
+  createTorrentDir()
+}
+
+// Start client
 var WebTorrent = require('webtorrent')
 var client = new WebTorrent()
-var exampleMagnetURI = "magnet:?xt=urn:btih:f59f3e4b2eb8be6e96148667ebbcc53343a13dc3&dn=The.Simpsons.S31E02.1080p.WEB.x264-TBS[rarbg]&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Feddie4.nl%3A6969&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969&tr=udp%3A%2F%2Fopentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337"
 
 
-//addTorrent(exampleMagnetURI)
+//Autoadd torrent on startup for debugging
+if (DEBUG) {
+  var exampleMagnetURI = "magnet:?xt=urn:btih:f59f3e4b2eb8be6e96148667ebbcc53343a13dc3&dn=The.Simpsons.S31E02.1080p.WEB.x264-TBS[rarbg]&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Feddie4.nl%3A6969&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969&tr=udp%3A%2F%2Fopentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337"
+  addTorrent(exampleMagnetURI)
+}
 
-//Homepage route
+
+//================================================================================
+// URL ROUTES
+//================================================================================
+
+ // HOME PAGE ROUTE
+ app.get('/', function (req, res) {
+  console.log("Get at /")
+
+  res.sendFile(path.join(__dirname,'/public/index.html'))
+});
+
+// API/INFO
 app.get('/api/info', (req,res) => {
     console.log("Get at /")
-
     Promise.all([getInfo()]).then(values => { 
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(values[0], null, 3));
     });   
 })     
 
-//Homepage route
+
+// /LIST
 app.get('/list', (req,res) => {
   console.log("Get at /list")
   Promise.all([getInfo()]).then(values => { 
-    //res.setHeader('Content-Type', 'application/json');
-    //res.end(JSON.stringify(values[0], null, 3));
-    //console.log(values[0])
-    //res.set('Content-Type', 'text/xml');
     var text=getRSS(values[0]);
+
     res.setHeader('Content-type', "application/octet-stream");
     res.setHeader('Content-disposition', 'attachment; filename=list.m3u8');
     res.send(text);
-
   });  
-
 })    
 
 
 
-//Download file route
+// DOWNLOAD FILE 
 app.get('/api/:infoHash/:fileNum/:filename', function(req, res){
   console.log("Get at /api/DOWNLOAD")
   let infoHash = req.params.infoHash
   let fileNum = req.params.fileNum
   let torrentObj = client.get(infoHash)
   let mypath = path.join(torrentObj.path,torrentObj.files[fileNum].path)
+
   res.sendFile(mypath)
-  
  }); 
 
- app.get('/', function (req, res) {
-  console.log("Get at /")
-  res.sendFile(path.join(__dirname,'index.html'))
-});
 
-//Add Torrent route
+// /API/ADD
 app.post('/api/add', urlencodedParser,(req,res) => {
   console.log("Post at /add, magnet: "+req.body.magnet)
   var magnetURI = req.body.magnet
   addTorrent(magnetURI)
-  //response
+
   res.redirect('/')
-  //res.setHeader('Content-Type', 'application/json');
-  //res.end(JSON.stringify({ response: 'ok' }, null, 3));
 })
 
-const uptime_api_key = 'u813039-dcc893686f8a5a616b09d557'
-const uptime_id = '783690536'
-
+// /API/ALIVE/ON
 app.get('/api/alive/on', function (req, res) {
   console.log("Get at /api/alive/on")
     axios.post('https://api.uptimerobot.com/v2/editMonitor', {
@@ -146,12 +170,13 @@ app.get('/api/alive/on', function (req, res) {
       //console.log(res.data)
     })
     .catch((error) => {
-      //console.error(error)
+      console.error(error)
     })
 
   res.redirect('/')
 });
 
+// /API/ALIVE/OFF
 app.get('/api/alive/off', function (req, res) {
   console.log("Get at /api/alive/off")
   axios.post('https://api.uptimerobot.com/v2/editMonitor', {
@@ -163,38 +188,30 @@ app.get('/api/alive/off', function (req, res) {
     //console.log(res.data)
   })
   .catch((error) => {
-    //console.error(error)
+    console.error(error)
   })
 
-res.redirect('/')
+  res.redirect('/')
 });
 
 
+// API/INFO
+app.get('/api/uptimestatus', (req,res) => {
+  console.log("Get at /")
+  Promise.all([getUptimeRobotStatus()]).then(values => { 
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(values[0], null, 3));
+  });   
+})    
 
 
 
-//Set static folder
-app.use(express.static(path.join(__dirname,'public'))) 
 
-
-//INICIO
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
-
-const HOSTNAME = process.env.HOSTNAME || 'http://miair.lan:8000';
-
-console.log("======================================")
-
-//addTorrent("magnet:?xt=urn:btih:04AF2550620D2322A01E1485F1A80B1A956EFB72&dn=%5Bzooqle.com%5D%20Game%20of%20Thrones%20S08E06%201080p%20WEB%20H264-MEMENTO%5Bettv%5D&tr=http://explodie.org:6969/announce&tr=http://announce.xxx-tracker.com:2710/announce&tr=http://tracker1.itzmx.com:8080/announce&tr=http://open.acgtracker.com:1096/announce&tr=https://bigfoot1942.sektori.org/announce&xl=4690910194&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker1.itzmx.com%3A8080%2Fannounce&tr=http%3A%2F%2Fannounce.xxx-tracker.com%3A2710%2Fannounce")
-
-
-
-//PROMISES
-
+//================================================================================
 //MEGA PROMISE FUNCTIONs
+//================================================================================
 
-//Promises que me dan info del sistema
+// Get free disk space in torrent folder
 function getFreeDiskSpace() {
   return new Promise((resolve, reject) => {
     checkDiskSpace(torrentDir).then((diskSpace) => {
@@ -203,7 +220,7 @@ function getFreeDiskSpace() {
   })
 }
 
-
+// Get MKV files in torrent folder
 function getMkvFiles() {
   return new Promise((resolve, reject) => {
     glob(torrentDir + '/**/*.+(mkv|avi)', {}, (err, files)=>{
@@ -212,6 +229,7 @@ function getMkvFiles() {
   })
 }
 
+// Get UptimeRobot monitor status
 function getUptimeRobotStatus() {
   return new Promise((resolve, reject) => {
     
@@ -231,6 +249,7 @@ function getUptimeRobotStatus() {
     })
 }
 
+// Get torrent info from webtorrent api
 function getTorrents(){
   let torrentSummary = []
   client.torrents.forEach( function (element){
@@ -238,7 +257,7 @@ function getTorrents(){
       name : element.name,
       infoHash : element.infoHash,
       done : element.done,
-      progress : element.progress*100,
+      progress : (element.progress*100).toFixed(2),
       numPeers : element.numPeers,
       paused : element.paused,
       path : element.path,
@@ -255,9 +274,7 @@ function getTorrents(){
   return torrentSummary
 }
 
-//Promise que me devuelve el resultado del promise.all de las infos del sistema
-// formateado en un objecto chachi a mi gusto
-
+// GetInfo function, returns promise with all info
 function getInfo() {
   return new Promise((resolve, reject) => {
     Promise.all([getFreeDiskSpace(),getMkvFiles()]).then(values => { 
@@ -273,54 +290,37 @@ function getInfo() {
 });
 }
 
+//================================================================================
 //FUNCTIONS
+//================================================================================
 
-
-
-
+//Creates and asigns torrentDir variable in tmp system folder
 function createTorrentDir(){
   fs.mkdtemp(path.join(os.tmpdir(), 'foo-'), (err, folder) => {
     if (err) throw err;
     console.log('Torrent destination folder: '+folder);
     torrentDir = folder
-    //addTorrent(exampleMagnetURI)
   });
-
-
 } 
   
-
-
+//addTorrent to webtorrent client
 function addTorrent(magnetURI){
   try{
     parseTorrent(magnetURI)
 
-
     client.add(magnetURI, { path: torrentDir }, function (torrent) {
-      torrent.on('done', function () {
-        console.log('torrent download finished')
-        })
-
-        torrent.on('error', (err) => {
-          console.log(err)
-        })
-      })
-    }
- 
+      torrent.on('done', () => {console.log('torrent download finished')})
+      torrent.on('error', (err) => {console.log(err)})
+    })
+  }
   catch (e){console.log(e)}
 }
   
-      
-
-    
-  
-  
-
-
+// Bytes to formated string
 function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
 
 
- 
+// Create m3u8 file with videos
 function getRSS(infoObj) {
   /* lets create an rss feed */
   let m3u8Content = ''
@@ -341,7 +341,7 @@ function getRSS(infoObj) {
   return m3u8Content
 }
 
-
+//Create objects with links to videos
 function makeLinks(torrentsInfo){
   let filesObj = []
   let fileObj = {}
@@ -350,17 +350,13 @@ function makeLinks(torrentsInfo){
       if (isVideo(path.basename(file))){
         fileObj = 
           { name: path.basename(file),
-            url: HOSTNAME+'/api/'+torrent.infoHash+'/'+index+'/file'+path.extname(file)
+            url: HOSTNAME+'/api/'+torrent.infoHash+'/'+index+'/'+path.basename(file)
           }
           filesObj.push(fileObj)
       }
-      
-      
     })
-    
   })
   return filesObj
-
 }
 
 
